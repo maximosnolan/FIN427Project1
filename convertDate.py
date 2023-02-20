1a106bcb577257e35203d90be8b8a52a999ed5d0
Maximos <maximosnolan@gmail.com>
2023-02-20 17:16:22 -0500

    updated

--- convertDate.py
+++ convertDate.py
@@ -1,6 +1,7 @@
 import pandas as pd
-
+from datetime import datetime
 data = pd.read_csv("dailyPrices.csv")
 
 data['DATE'] = data['date'].str[0:2] + '-' + data['date'].str[2:5] + '-' + data['date'].str[7:9]
+data['Month'] = datetime.strptime(data['date'].str[2:5].str(), '%b').month
 data.to_csv("dailyPrices.csv")
